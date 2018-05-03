a16baac6ed5d981eb2376e05d9a1da1a1336f889
Jiro Kirameki <jiro.kirameki@techacademy.jp>
2018-05-03 13:49:46 +0000

    CRUD tasks

--- database/migrations/2018_05_02_155204_create_tasks_table.php
+++ database/migrations/2018_05_02_155204_create_tasks_table.php
@@ -13,11 +13,11 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('content');
-            $table->timestamps();
-        });
+         Schema::create('tasks', function (Blueprint $table) {
+             $table->increments('id');
+             $table->string('content');
+             $table->timestamps();
+         });
     }
 
     /**
