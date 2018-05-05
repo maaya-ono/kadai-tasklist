2c2c25b9497865c0fba4c7b7907e9b6222d4c1ee
EC2 Default User <ec2-user@ip-172-31-8-8.us-east-2.compute.internal>
2018-05-05 09:03:37 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,6 +8,9 @@
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
+        
+        {!! Form::label('status', '状態:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::submit('作成') !!}
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,11 +3,14 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
-
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    
+        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
+        
+        {!! Form::label('status', '状態:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::submit('更新') !!}
 
